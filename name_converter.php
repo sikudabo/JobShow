47f3a8c564502c3a60bd1daee2fc9719c052283d
Simeon Ikudabo <32518831+sikudabo@users.noreply.github.com>
2018-08-29 09:41:25 -0400

    Add files via upload
    This program demonstrates that using the clone method can enable us to clone an object and enable the second object to be its own individual object. If we did not use the clone method, the two objects would reference the same object and changing the properties of one object would change the properties of another. This is shown in PHP.

--- name_converter.php
+++ name_converter.php
@@ -1,25 +1,25 @@
 <?php
 
+/*Create an object using the clone method. The clone method will copy the object and we can 
+set it as the value of another object. However, they will still be two seperate objects. The 
+output of our program will prove that they are two seperate objects because the name property 
+for obj1 and obj2 will vary. If we set obj2 = to obj1 without the clone method they would reference
+the same object and changing the property of one object would automatically change the property of another */
 
-$currentUser = new User("Simeon", "pass");
-print_r($currentUser);
-echo "<br>";
-$currentUser->save_user();
 
 
 
-class User{
+$obj1 = new Obj;
+$obj1->name = "Simeon";
+$obj2 = clone $obj1; //If we didn't clone obj1, obj2 would reference the same object 
+$obj2->name = "Sim";
+echo "obj 1 name = $obj1->name <br>";
+echo "obj 2 name = $obj2->name";
 
-	public $username, $password;
 
-	function User($username, $password){
-		$this->username = $username;
-		$this->password = $password;
-		}
 
-	function save_user(){
-		echo "User $this->username is saved. Their password is $this->password";
-		}
+class Obj{
+	public $name;
 	}
 
 
